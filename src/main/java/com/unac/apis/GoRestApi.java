9dbe7df7c5fe1b714fd03f55dad723a51082b0d6
DavidSanchez055 <davidf.sanchezm@una.edu.co>
2024-05-22 23:58:48 -0500

    first commit

--- src/main/java/com/unac/apis/GoRestApi.java
+++ src/main/java/com/unac/apis/GoRestApi.java
@@ -4,7 +4,7 @@ import com.unac.restinteractions.RestInteractions;
 
 public class GoRestApi {
 
-    private static final String TOKEN = "e871d08829b6c03f39b4eea4d1e51522ab2cf5b59685c8d082f196db681b6254";
+    private static final String TOKEN = "6b2783938ff4fa138d22b6c93438f4d96e6430a29fe7027123e10f9934e51824";
     private static String pathName = "name";
 
     public static void setBaseUrl(String baseUrl) {
--- src/test/java/com/unac/runners/GetGoRest.java
+++ src/test/java/com/unac/runners/GetGoRest.java
@@ -7,7 +7,7 @@ import org.junit.runner.RunWith;
 @RunWith(CucumberWithSerenity.class)
 @CucumberOptions(
         glue = "com.unac.stepsdefinitions",
-        features = "src/test/resources/com/unac/get_go_rest.feature",
+        features = "src/test/resources/com/unac/features/get_go_rest.feature",
         snippets = CucumberOptions.SnippetType.CAMELCASE
 )
 public class GetGoRest {
